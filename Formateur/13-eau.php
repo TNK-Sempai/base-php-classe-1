bbba124af0b067e71e21852cc307d701fbbb9466
WebDevCF2m <gitweb@cf2m.be>
2026-03-13 12:21:00 +0100

    git commit -m'Create Formateur/14-pair.php'

--- Formateur/13-eau.php
+++ Formateur/13-eau.php
@@ -41,5 +41,36 @@
 
     ?>
     <h4><?=$string?></h4>
+    <pre><code>
+    // on mets dans température un int entre -100 et 200
+    // depuis PHP 7.1 rand est l'alias de mt_rand
+    $temperature = rand(-100,200); 
+
+    // création d'une chaîne de caractère commune aux 3 états
+    $string = "L'eau à $temperature"."° est ";
+
+    # conditions if / elseif / else avec {}
+
+    // si la température est plus petit ou égale à 0
+    if($temperature <= 0){
+
+        // concaténation de $string avec le .=
+        $string .= "solide"; // l'eau est solide
+
+    // sinon si la température est inférieure à 100   
+    }elseif($temperature < 100){
+
+        // concaténation de $string avec le .=
+        $string .= "liquide"; // l'eau est liquide
+
+    // sinon reste de 100 à 200   
+    }else{
+
+        // concaténation de $string avec le .=
+        $string .= "gazeuse"; // l'eau est gazeuse
+
+    }
+</code></pre>
+<h4><?=$string?></h4>
 </body>
 </html>
